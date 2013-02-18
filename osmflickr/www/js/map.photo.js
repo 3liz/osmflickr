5c791e632c8bb8e7b5159563113ddf2bad1956a0
D'Hont René-Luc <rldhont@gmail.com>
2013-02-18 10:49:13 +0100

    Can select OpenStreetMap element tagged
    In the OpenStreetMap tagged layer, the user can select and consulte the
    OpenStreetMap element.

--- osmflickr/www/js/map.photo.js
+++ osmflickr/www/js/map.photo.js
@@ -47,11 +47,20 @@ ofMap.events.on({
 
     var osmtagLayer = new OpenLayers.Layer.Vector('osmtag', {
       styleMap: new OpenLayers.StyleMap({
-        pointRadius: 6,
-        fillColor: "#0063DC",
-        fillOpacity: 0.4,
-        strokeColor: "#FF0084",
-        strokeWidth: 1
+        "default": new OpenLayers.Style({
+          pointRadius: 6,
+          fillColor: "#0063DC",
+          fillOpacity: 0.4,
+          strokeColor: "#FF0084",
+          strokeWidth: 1
+        }),
+        "select": new OpenLayers.Style({
+          pointRadius: 8,
+          fillColor: "#0063DC",
+          fillOpacity: 1,
+          strokeColor: "#FF0084",
+          strokeWidth: 2
+        })
       })
     });
     map.addLayer(osmtagLayer);
@@ -118,11 +127,80 @@ ofMap.events.on({
           }
         });
     var selectOsm = new OpenLayers.Control.SelectFeature(
-        osmvector,
+        [osmtag, osmvector],
         {includeXY:true}
         );
     map.addControls([drawBox,selectOsm]);
     selectOsm.activate();
+    osmtag.events.includeXY = true;
+    osmtag.events.on({
+      "featureselected": function(evt) {
+        var feat = evt.feature;
+        var fid = feat.fid.split('.');
+        var text = '<h4>OSM element '+fid[0]+'</h4>';
+        text += '<div class="lizmapPopupDiv">';
+        text += '<table class="lizmapPopupTable">';
+        text += '<thead><tr><th class="left">key</th><th>value</th></tr></thead>';
+        text += '<tbody>';
+        var urlRegex = /^(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+        var emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/;
+        var imageRegex = /\.(jpg|jpeg|png|gif|bmp)$/i;
+        var attr = feat.attributes;
+        for (var k in attr) {
+          var v = attr[k];
+          if ( imageRegex.test(v) )
+      text += '<tr><td colspan="2"><img src="'+v+'" width="300" border="0"/></td></tr>';
+          else {
+            text += '<tr><th>'+k+'</th>';
+            if ( urlRegex.test(v) )
+      text += '<td><a href="'+v+'" target="_blank">'+v+'<a></td></tr>';
+            else if ( emailRegex.test(v) )
+              text += '<td><a href="mailto:'+v+'">'+v+'</a></td></tr>';
+            else
+              text += '<td>'+v+'</td></tr>';
+          }
+        }
+        text += '</tbody>';
+        text += '</table>';
+        text += '</div>';
+
+        if (map.popups.length != 0)
+          map.removePopup(map.popups[0]);
+
+        OpenLayers.Popup.ofMapAnchored = OpenLayers.Class(OpenLayers.Popup.Anchored, {
+          'displayClass': 'olPopup lizmapPopup'
+          ,'contentDisplayClass': 'olPopupContent lizmapPopupContent'
+        });
+        var popup = new OpenLayers.Popup.ofMapAnchored(
+            "liz_layer_popup",
+            map.getLonLatFromPixel(selectOsm.handlers.feature.down),
+            null,
+            text,
+            null,
+            true,
+            function() {
+              map.removePopup(this);
+              selectOsm.unselectAll();
+            }
+            );
+        popup.panMapIfOutOfView = true;
+        popup.autoSize = true;
+        popup.maxSize = new OpenLayers.Size(450, 400);
+        map.addPopup(popup);
+        // correcting the height
+        var contentDivHeight = 0;
+        $('#liz_layer_popup_contentDiv').children().each(function(i,e) {
+          contentDivHeight += $(e).outerHeight(true);
+        });
+        if ( $('#liz_layer_popup_contentDiv').height() > contentDivHeight ) {
+          $('#liz_layer_popup_contentDiv').height(contentDivHeight)
+            $('#liz_layer_popup').height(contentDivHeight)
+        }
+        if($('#liz_layer_popup').height()<contentDivHeight) {
+          $('#liz_layer_popup .olPopupCloseBox').css('right','14px');
+        }
+      }
+    });
     osmvector.events.includeXY = true;
     osmvector.events.on({
       "sketchcomplete": function(evt) {
@@ -139,8 +217,8 @@ ofMap.events.on({
           var osmLayer = map.getLayersByName('osmvector')[0];
           var osmFormat = new OpenLayers.Format.OSM({
             checkTags:true,
-            externalProjection: new OpenLayers.Projection('EPSG:4326'),
-            internalProjection: map.getProjectionObject()
+              externalProjection: new OpenLayers.Projection('EPSG:4326'),
+              internalProjection: map.getProjectionObject()
           });
           var osmFeats = osmFormat.read(data);
           var feats = [];
@@ -160,7 +238,7 @@ ofMap.events.on({
           feats.sort(function(a, b) {
             if (a.geometry.CLASS_NAME == b.geometry.CLASS_NAME) {
               if (a.geometry.CLASS_NAME == 'OpenLayers.Geometry.Point')
-                return 0;
+            return 0;
               if (a.geometry.CLASS_NAME == 'OpenLayers.Geometry.LineString')
                 return 0;
               return b.geometry.getArea() - a.geometry.getArea();
@@ -190,7 +268,7 @@ ofMap.events.on({
           var fid = feat.fid.split('.');
           var text = '<h4>OSM element '+fid[0]+'</h4>';
           text += '<div class="lizmapPopupDiv">';
-          text += '<button class="btn osmtag-add" title="Add"><span class="icon"></span><span class="text">'+dict['osm.tagmachine.add']+'</span></button>'
+          text += '<button class="btn osmtag-add" title="Add"><span class="icon"></span><span class="text">'+dict['osm.tagmachine.add']+'</span></button>';
           text += '<table class="lizmapPopupTable">';
           text += '<thead><tr><th class="left">key</th><th>value</th></tr></thead>';
           text += '<tbody>';
